f3dddd36605ca00251be67438878402093987b16
Jia-Yi, Jian <jiayi.jian95@gmail.com>
2019-12-04 17:34:15 -0600

    add data into database

--- recipeinfo/models.py
+++ recipeinfo/models.py
@@ -5,6 +5,11 @@ class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(max_length=45, unique=True)
 
+    def __str__(self):
+        return '%s' % self.category_name
+
+    class Meta:
+        ordering = ['category_name']
 
 class Ingredient(models.Model):
     ingredient_id = models.AutoField(primary_key=True)
@@ -13,7 +18,7 @@ class Ingredient(models.Model):
     category = models.ForeignKey(Category, related_name='ingredients', on_delete=models.PROTECT)
 
     def __str__(self):
-        return '%s' % self.ingredient_name
+        return '%s' % ((self.ingredient_name))
 
     class Meta:
         ordering = ['ingredient_name']
@@ -36,10 +41,10 @@ class Beverage(models.Model):
     beverage_type = models.CharField(max_length=45, default='')
 
     def __str__(self):
-        return '%s - %s' % (self.beverage_type, self.beverage_name)
+        return '%s' % (self.beverage_name)
 
     class Meta:
-        ordering = ['beverage_type']
+        ordering = ['beverage_name']
 
 
 class Recipe(models.Model):
@@ -61,9 +66,20 @@ class Recipe_Ingredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, related_name='recipe_ingredients', on_delete=models.PROTECT)
     recipe = models.ForeignKey(Recipe, related_name='recipe_ingredients', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return '%s: %s' % ((self.recipe, self.ingredient))
+
+    class Meta:
+        ordering = ['recipe']
+
 
 class Beverage_Ingredient(models.Model):
     beverage_ingredient_id = models.AutoField(primary_key=True)
     ingredient = models.ForeignKey(Ingredient, related_name='beverage_ingredients', on_delete=models.PROTECT)
     beverage = models.ForeignKey(Beverage, related_name='beverage_ingredients', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return '%s: %s' % ((self.beverage, self.ingredient))
+
+    class Meta:
+        ordering = ['beverage']
