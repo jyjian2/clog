5adf7f3c7d99344e590110c8935fd462e41f764f
Jia-Yi, Jian <jiayi.jian95@gmail.com>
2019-12-09 16:46:07 -0600

    move template file under recipeinfo

--- jian_jiayi_final_project/settings.py
+++ jian_jiayi_final_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'jian_jiayi_final_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'recipeinfo/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
