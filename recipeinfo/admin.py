006155e704dd59a3f6b034829eb2bae376ae2bde
Jia-Yi, Jian <jiayi.jian95@gmail.com>
2019-12-11 16:36:39 -0600

    dessert ing

--- recipeinfo/admin.py
+++ recipeinfo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, Recipe_Ingredient, Beverage, Category, Beverage_Ingredient, Recipe_Type
+from .models import Ingredient, Recipe, Recipe_Ingredient, Beverage, Category, Beverage_Ingredient, Recipe_Type, Dessert, Dessert_Ingredient
 
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
@@ -8,4 +8,6 @@ admin.site.register(Beverage_Ingredient)
 admin.site.register(Category)
 admin.site.register(Beverage)
 admin.site.register(Recipe_Type)
+admin.site.register(Dessert)
+admin.site.register(Dessert_Ingredient)
 # Register your models here.
